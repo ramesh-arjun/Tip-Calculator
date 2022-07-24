624088eb11dc5ec91e1cefeccd40adb9d8ec0fd7
Arjun R <103382747+ramesh-arjun@users.noreply.github.com>
2022-07-23 20:32:36 -0500

    Update AccountRepository.java

---
+++ AccountRepository.java
@@ -0,0 +1,7 @@
+    package com.tip.calc.data;
+import org.springframework.data.repository.CrudRepository;  
+
+public interface AccountRepository extends CrudRepository<AccountEntity,Integer>{
+
+}
+    
